c8fcf8e00a06f615e1550088584e38a23f08a01f
Shahbaz Ansari <mdshahbaz8298@gmail.com>
2025-08-27 19:55:37 +0530

    Logo fixed

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -727,17 +727,17 @@ const Header = () => {
     <>
       <header className={`header left-0 top-0 z-40 w-full items-center  ${sticky ? "fixed bg-[#ffffff] text-black  shadow-lg backdrop-blur-2xl " : "absolute bg-[#ffffff]  "} transition-all`}>
         <div className="flex justify-between items-center px-5 py-4 max-w-7xl mx-auto w-full">
-          <Link href="/" legacyBehavior>
-            <a>
-              <Image
-                src="/images/logo/Leads2crm.jpg"
-                alt="Leads2CRM Logo"
-                width={160}
-                height={60}
-                priority
-                className="z-40 mt-[-1px]" // Optional: adjust styles
-              />
-            </a>
+          <Link href="/">
+            {/* <a> */}
+            <Image
+              src="/images/logo/Leads2crm.jpg"
+              alt="Leads2CRM Logo"
+              width={160}
+              height={60}
+              // priority
+              className="z-40 mt-[-1px]" // Optional: adjust styles
+            />
+            {/* </a> */}
           </Link>
 
           <button
