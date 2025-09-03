bf53ff26d2ca450e2ef31dc7bb3edb7068de9f86
Shahbaz Ansari <mdshahbaz8298@gmail.com>
2025-09-03 20:33:57 +0530

    google analytics done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,6 +54,7 @@ export default function RootLayout({
           strategy="afterInteractive"
           src="https://www.googletagmanager.com/gtag/js?id=G-SR0XTSY1B3"
         />
+        
         <Script id="google-analytics" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
@@ -64,11 +65,34 @@ export default function RootLayout({
             });
           `}
         </Script>
+
+        {/* <!-- Google Tag Manager --> */}
+        {/* GTM Head Snippet */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id=GTM-KWPB4NGN'+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KWPB4NGN');
+          `}
+        </Script>
+        {/* <!-- End Google Tag Manager --> */}
       </head>
 
       {/* <body className={`bg-[#FCFCFC] dark:bg-black ${inter.className}`}> */}
       <body className={`bg-[#FFFFFF]  ${inter.className}`}>
-
+        {/* <!-- Google Tag Manager (noscript) --> */}
+        {/* GTM Body Snippet */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KWPB4NGN"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* <!-- End Google Tag Manager (noscript) --> */}
 
         <Providers>
           <AnalyticsListener />
