2ff60558b42aeb7fb9a3f2ccfe5d821fbd318f42
Shahbaz Ansari <mdshahbaz8298@gmail.com>
2025-09-28 21:33:05 +0530

    remove loader

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,21 +2,21 @@
 
 import { ThemeProvider } from "next-themes";
 import { useEffect, useState } from "react";
-import Loader from "./Loader";
+// import Loader from "./Loader";
 
 export function Providers({ children }: { children: React.ReactNode }) {
 
-  const [loading, setLoading] = useState(true);
+  // const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    // Show loader for 3s (customizable)
-    const timer = setTimeout(() => setLoading(false), 3000);
-    return () => clearTimeout(timer);
-  }, []);
+  // useEffect(() => {
+  //   // Show loader for 3s (customizable)
+  //   const timer = setTimeout(() => setLoading(false), 3000);
+  //   return () => clearTimeout(timer);
+  // }, []);
 
-  if (loading) {
-    return <Loader />; // show loader until hydration
-  }
+  // if (loading) {
+  //   return <Loader />; // show loader until hydration
+  // }
 
   return (
     <ThemeProvider attribute="class" enableSystem={false} defaultTheme="dark">
